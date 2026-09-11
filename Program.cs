ae719c1601024c700fd138636246ec331b385aa0
Veronika <gavrilova.veronika38d@gmail.com>
2026-09-11 17:28:40 +0300

    Lab01: variables and interpolation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string myName = "Гаврилова Вероника";
+string groupName = "ИСП-251";
+int courseNumber = 2;
+double averageGrade = 4.6;
+bool isBudget = true;
+
+Console.WriteLine( "Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"[pynna: {groupName}");
+Console.WriteLine($"Kypc: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");
